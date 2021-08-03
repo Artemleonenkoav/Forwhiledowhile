f1b9a7848d313b3e103326f2a4c9b247f0a655b7
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-08-03 17:45:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 9; i++){
+            System.out.printf("Квадрат числа %d равен %d \n", i, i * i);
+        }
+    }
+}
